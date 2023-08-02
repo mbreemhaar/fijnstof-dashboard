f7aa0a3107f87007a49f90a2b2fe899015b0364a
Marco Breemhaar <marcobreemhaar@gmail.com>
2023-08-02 16:11:17 +0200

    Automatically delete old observations

--- observations/get_data.py
+++ observations/get_data.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 import pytz
 import requests
-from django.utils.timezone import make_aware
+from django.utils.timezone import make_aware, now
 
 from observations.models import Sensor, Observation
 
@@ -19,6 +19,8 @@ def get_data():
 
     country_filtered_data = filter_country(response_data, country_code='NL')
     add_sensors(country_filtered_data)
+    add_observations(country_filtered_data)
+    delete_old_observations()
 
 
 def add_sensors(data):
@@ -35,24 +37,32 @@ def add_sensors(data):
 def add_observations(data):
     observations = []
     for row in data:
-        timestamp = make_aware(datetime.strptime(row['timestamp'], '%Y-%m-%d %H-%M-%S'), pytz.timezone('UTC'))
+        timestamp = make_aware(datetime.strptime(row['timestamp'], '%Y-%m-%d %H:%M:%S'), pytz.timezone('UTC'))
 
-        for observation_data in row:
+        for observation_data in row['sensordatavalues']:
             observation_type = TYPE_MAP.get(observation_data['value_type'])
 
             if observation_type is None:
+                continue
+
+            value = round(float(observation_data['value']), 3)
+            if value > 999:
                 continue
 
             observations.append(
                 Observation(
                     timestamp=timestamp,
                     type=observation_type,
-                    value=float(observation_data['value']),
-                    sensor=Sensor.objects.get(sensor_community_id=int(row['sensor']))
+                    value=value,
+                    sensor=Sensor.objects.get(sensor_community_id=int(row['sensor']['id']))
                 )
             )
 
     Observation.objects.bulk_create(observations)
+
+
+def delete_old_observations(delta=timedelta(minutes=5)):
+    Observation.objects.filter(timestamp__lt=now()-delta).delete()
 
 
 def filter_country(data, country_code='NL'):
